7a38a5dd3c736a20a8da1e834cb336cbe7eb8d1b
Ckenmuk <dr.dolohoff@yandex.ru>
2023-07-02 15:44:31 +0600

    123

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,24 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Decode(0) = " + (Decode("0"))); // 0
+            Console.WriteLine("Decode(123) = " + (Decode("123"))); // 123
+            Console.WriteLine("Decode(1.23) = " + (Decode("1.23"))); // 123
+            Console.WriteLine("Decode(1...2..3) = " + (Decode("1...2..3"))); //123
+            Console.WriteLine("Decode(1010) = " + (Decode("1010"))); // 1010
+            Console.WriteLine("Decode(1025) = " + (Decode("1025"))); // 1
+            Console.WriteLine("Decode(1..02.6) = " + (Decode("1..02.6"))); // 2
+
+
+        }
+
+        private static string Decode(string a)
+        {
+            a = a.Replace(".", "");
+
+            int b = int.Parse(a);
+
+                return (b % 1024).ToString();
         }
     }
 }
